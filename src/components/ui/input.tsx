121e039f8188ff3658c2529aea031d871aa6c99c
Mohammed <moemad191@gmail.com>
2024-11-06 13:28:28 +0300

    fix interface declaring in input and textarea 2

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,25 @@
 import * as React from "react";
 import { cn } from "@/lib/utils";
 
-const Input = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      type={type}
-      className={cn(
-        "flex h-[48px] rounded-md border border-white/10 focus:border-accent font-light bg-primary px-4 py-5 text-base placeholder:text-white/60 outline-none ",
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-[48px] rounded-md border border-white/10 focus:border-accent font-light bg-primary px-4 py-5 text-base placeholder:text-white/60 outline-none ",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 Input.displayName = "Input";
 
 export { Input };
