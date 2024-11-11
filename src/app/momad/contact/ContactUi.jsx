d8d994415e2123951b7fc9fb1c901fb7664701ac
Mohammed <moemad191@gmail.com>
2024-11-11 10:35:57 +0300

    type=textarea

--- src/app/momad/contact/ContactUi.jsx
+++ src/app/momad/contact/ContactUi.jsx
@@ -190,7 +190,6 @@ const ContactUi = () => {
               {/* textarea */}
               <textarea
                 className="h-[200px] flex min-h-[80px] w-full rounded-md border border-white/10 bg-primary px-4 py-5 text-base placeholder:text-white/60 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-accent focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50"
-                type="textarea"
                 placeholder="type your message here"
                 name="message"
                 id="message"
