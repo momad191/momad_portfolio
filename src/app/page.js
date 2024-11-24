ed55d2bc26f7aad12e39b814bb45ad0ccfd29b0c
Mohammed Emad <moemad191@gmail.com>
2024-11-24 22:19:03 +0300

    add omer to nam

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ const Home = () => {
             <h1 className="h1 mb-6">
               {" "}
               Hello I am <br />{" "}
-              <span className="text-accent">Mohammed Emad </span>{" "}
+              <span className="text-accent">Mohammed Emad Omer </span>{" "}
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80 ">
               {" "}
